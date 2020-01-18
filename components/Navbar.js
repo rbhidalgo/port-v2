fadff5443c46df790b90f8c8a03a4e7685b7935f
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-01-18 03:03:39 -0800

    initial setup

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+
+const Navbar = () => (
+    <nav>
+        <ul>
+        <li><Link href="/"><a>Home</a></Link></li>
+    <li><Link href="/about"><a>About</a></Link></li>
+        </ul>
+    </nav>
+)
+
+export default Navbar;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar'
+
+
+const About = () => (
+    <div>
+        <Navbar />
+        <h1>About Me</h1></div>
+)
+
+
+export default About
