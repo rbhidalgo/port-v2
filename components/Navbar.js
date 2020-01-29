20ba06085b1fa9dcf099ac02a305032df64fde23
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-01-28 23:10:32 -0800

    add social icons and css styling for nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,8 +6,17 @@ const Navbar = () => (
         <ul>
         <li><Link href="/"><a>Home</a></Link></li>
     <li><Link href="/about"><a>About</a></Link></li>
+    <li><Link href="/projects"><a>Projects</a></Link></li>
         </ul>
+        <img src="/img/github.svg"/>
+        <img src="/img/linkedin.svg"/>
+        <img src="/img/docicon.svg"/>
+        
 <style jsx>{`
+    nav {
+        display: flex;
+        width: 100%;
+    }
     ul {
         background: #333;
         color: #fff;
@@ -23,6 +32,9 @@ const Navbar = () => (
     ul li a {
         color: #fff;
         text-decoration: none;
+    }
+    nav img{
+        width: 60px;
     }
 `}</style>
     </nav>
---
+++ pages/projects.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout'
+
+
+const Projects = () => (
+    <Layout>
+        <>
+        <h1>Projects Me</h1>
+        </>
+    </Layout>
+)
+
+
+export default Projects
