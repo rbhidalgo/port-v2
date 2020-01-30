68af1d53e2fb766b3f0c65594fa323aaafb433fa
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-01-30 00:53:22 -0800

    update project links in projects.json and dispaly them on project.js component

--- components/Project.js
+++ components/Project.js
@@ -1,21 +1,31 @@
 import Link from 'next/link'
 
-const Project = ({image, name, description, tech, github, live, responsive}) => {
+const Project = ({image, name, description1, description2, tech, githubLink, siteLink, responsive}) => {
     // const backgroundImg = {
     //         width: "70%",
     //         height: "400px",
     //         backgroundImage: `url("${props.image}")`,
 
     // }
+    const techItems = tech.map((tech) =>
+    <li>{tech}</li> )
+
     return (
         <ul>
-        <li><div className="project-img" style={{backgroundImage: `url("${image}")`}}></div>
+        <li className="project-item"><div className="project-img" style={{backgroundImage: `url("${image}")`}}></div>
         {/* <img src={props.image} alt="`screenshot of project ${props.name}`" /> */}
         <h1>
             {name}
             </h1>
-    <p>{description}</p>
-           
+    <p>{description1}</p>
+    <p>{description2}</p>
+                <ul className="project-tech-tags">
+                   {techItems}
+                </ul>
+    <a href={githubLink} target="_blank">Github Link</a>
+    <a href={siteLink} target="_blank">Site Link</a>
+
+
             </li>
             <style jsx>{`
                 .project-img {
