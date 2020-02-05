2644eb2c00e5a3a80301fdfe35d5a4d1b2ed1aa4
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-02-04 23:11:45 -0800

    add intro text content

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,11 @@ import projects from './projects.json'
 const Index = () => (
     <Layout title="Full Stack Web Developer | Rogelio Hidalgo | Los Angeles, CA">
     <section className="intro">
-    <h1>Hi!</h1>
+    <p>Rogelio Hidalgo, Full Stack</p>
+    <h1>Web Developer</h1>
+    <p>with a creative edge</p>
+    <p>focusing on front-end development with the ability to be flexible and adapt to various projects. My knowledge in full-stack development paired with my experience in graphic design allows me to build applications that are aesthetically pleasing and functional. I look forward to working with a team that values continual growth and development.</p>
+    <a href="#">get in touch</a> <a href="#">view my portfolio</a>
     </section>
     <section className="projects-container">
     {projects.map((project, index) => {
