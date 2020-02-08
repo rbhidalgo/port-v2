041f2da439a5114120a42765eb17b356f1437a7e
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-02-07 21:56:53 -0800

    create IntroSection component

---
+++ components/IntroSection.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const IntroSection = () => {
+
+    return (
+        <div>
+           
+            <style jsx>{`
+                
+
+                `}
+            </style>
+        </div>
+    )
+}
+
+export default IntroSection
