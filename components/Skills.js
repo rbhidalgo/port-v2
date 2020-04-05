506bda26dac13bd596c95e19aa6fd0c48bbcd1cc
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-05 02:17:46 -0700

    create skills component

---
+++ components/Skills.js
@@ -0,0 +1,18 @@
+
+const Skills = () => {
+
+    return (
+       <section className="section-skills">
+         <div className="skills-text-heading">
+
+         </div>
+
+        <div className="skills-text-list">
+          
+        </div>
+
+       </section>
+    )
+}
+
+export default Skills
