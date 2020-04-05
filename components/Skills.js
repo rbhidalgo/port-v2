a5cd884d9cb8fb3d8b491ef85cbaec452df4af4b
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-05 02:30:02 -0700

    create skills array and map it in skills.js component

--- components/Skills.js
+++ components/Skills.js
@@ -1,14 +1,50 @@
 
 const Skills = () => {
 
-    return (
-       <section className="section-skills">
-         <div className="skills-text-heading">
+    const skills = [
+      "HTML5",
+      "CSS3",
+      "Javascript",
+      "Vanilla JS", 
+      "MongoDB",
+      "Express",
+      "React",
+      "Node.js",
+      "Next.js",
+      "Mongoose",
+      "Restful Routing",
+      "AJAX",
+      "JSON",
+      "GIT",
+      "Github",
+      "Bootstrap",
+      "tachyons",
+      "Wordpress",
+      "WooCommerce",
+      "Adobe Creative Cloud Suite",
+      "Adobe XD",
+      "Dreamweaver",
+      "Photoshop",
+      "Illustrator",
+      "Indesign"
+      ]
 
+      const skillItem = skills.map((skill, i ) => {
+        <li key={i}>
+            {skill}
+        </li>
+      })
+
+    return (
+       <section className="section-skills grid">
+         <div className="skills-text-heading">
+            <h1>Skills</h1>
          </div>
 
         <div className="skills-text-list">
-          
+          <ul>
+            {skillItem}
+          </ul>
         </div>
 
        </section>
