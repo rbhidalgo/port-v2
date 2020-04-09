e63fd460ebdb415cb08dd4bd4ec5fdc29e3758f7
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-09 01:30:37 -0700

    re-structure project item component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,75 +1,73 @@
 import Link from 'next/link'
 
-
 const Navbar = () => (
-    <header>
-        <nav>
-        <ul className="nav-links">
-        <li><Link href="/"><a>Home</a></Link></li>
+	<header>
+		<nav>
+			<ul className='nav-links'>
+				<li>Download My Resume</li>
+				{/* <li><Link href="/"><a>Home</a></Link></li>
     <li><Link href="/about"><a>About</a></Link></li>
-    <li><Link href="/projects"><a>Projects</a></Link></li>
-        </ul>
-        <div className="nav-right-socials">
-        <img src="/socials/bxs-file-pdf.svg"/>
-        <img src="/socials/bxl-github.svg"/>
-        <img src="/socials/bxl-linkedin-square.svg"/>
-        <img src="/socials/bxl-dribbble.svg"/>
-        </div>
-        </nav>
-<style jsx>{`
-    nav {
-        display: flex;
-        max-width: 1366px;
-        margin: 0 auto;
-        min-height: 7.5rem;
-        align-items: center;
-        justify-content: space-evenly;
-        text-transform: uppercase;
-        color: #333;
-    }
-    header {
-        
-        background: #fff;
-        position: sticky;
-        top: 0;
-        z-index: 10;
-    }
+    <li><Link href="/projects"><a>Projects</a></Link></li> */}
+			</ul>
+			<div className='nav-right-socials'>
+				<img src='/socials/bxs-file-pdf.svg' />
+				<img src='/socials/bxl-github.svg' />
+				<img src='/socials/bxl-linkedin-square.svg' />
+				<img src='/socials/bxl-dribbble.svg' />
+			</div>
+		</nav>
+		<style jsx>{`
+			nav {
+				display: flex;
+				max-width: 1366px;
+				margin: 0 auto;
+				min-height: 7.5rem;
+				align-items: center;
+				justify-content: space-evenly;
+				text-transform: uppercase;
+				color: #333;
+			}
+			header {
+				background: #fff;
+				position: sticky;
+				top: 0;
+				z-index: 10;
+			}
 
-    ul.nav-links {
-        flex: 1;
-        list-style: none;
-        display: flex;
-        margin-left: 6.4rem;
-    }
-    ul li {
-        font-size: 1.8rem;
-        margin-right: 2.4rem;
-        font-family: presicav, sans-serif;
-font-weight: 900;
-font-style: normal;
-    }
+			ul.nav-links {
+				flex: 1;
+				list-style: none;
+				display: flex;
+				margin-left: 6.4rem;
+			}
+			ul li {
+				font-size: 1.8rem;
+				margin-right: 2.4rem;
+				font-family: presicav, sans-serif;
+				font-weight: 900;
+				font-style: normal;
+			}
 
-    ul li a {
-        color: #333;
-        text-decoration: none;
-    }
-    .nav-right-socials {
-        display: flex;
-    flex: 1;
-    justify-content: flex-end;
-    margin-right: 6.4rem;
-    }
-    .nav-right-socials img {
-        width: 2.4rem;
-        margin-right:  1.8rem;
-    }
+			ul li a {
+				color: #333;
+				text-decoration: none;
+			}
+			.nav-right-socials {
+				display: flex;
+				flex: 1;
+				justify-content: flex-end;
+				margin-right: 6.4rem;
+			}
+			.nav-right-socials img {
+				width: 2.4rem;
+				margin-right: 1.8rem;
+			}
 
-    .nav-right-socials img:last-child {
-        margin-right: 0;
-    }
-
-`}</style>
-    </header>
+			.nav-right-socials img:last-child {
+				margin-right: 0;
+			}
+		`}</style>
+	</header>
 )
 
-export default Navbar;
+export default Navbar
--- components/Project.js
+++ components/Project.js
@@ -1,36 +1,53 @@
 import Link from 'next/link'
-import { motion, AnimatePresence } from "framer-motion"
+import { motion, AnimatePresence } from 'framer-motion'
 
-const Project = ({ index, image, name, description1, description2, tech, githubLink, siteLink, responsive }) => {
-    // const backgroundImg = {
-    //         width: "70%",
-    //         height: "400px",
-    //         backgroundImage: `url("${props.image}")`,
+const Project = ({
+	index,
+	image,
+	name,
+	description1,
+	description2,
+	tech,
+	githubLink,
+	siteLink,
+	responsive,
+}) => {
+	// const backgroundImg = {
+	//         width: "70%",
+	//         height: "400px",
+	//         backgroundImage: `url("${props.image}")`,
 
-    // }
-    const techItems = tech.map((tech, i) =>
-        <li key={i} style={{margin:"12px"}}>{tech}</li>)
+	// }
+	const techItems = tech.map((tech, i) => (
+		<li key={i} style={{ margin: '12px' }}>
+			{tech}
+		</li>
+	))
 
-    return (
-            <li className="project-item" key={index}>
-                <div className="project-img" style={{ backgroundImage: `url("${image}")` }}></div>
-                {/* <img src={props.image} alt="`screenshot of project ${props.name}`" /> */}
-                <div className="project-content">
-                    <h1>
-                        {name}
-                    </h1>
-                    <p>{description1}</p>
-                    <p>{description2}</p>
-                    <ul className="project-tech-tags">
-                        {techItems}
-                    </ul>
-                    <a href={githubLink} target="_blank">Github Link</a>
-                    <a href={siteLink} target="_blank">Site Link</a>
+	return (
+		<li className='project-item' key={index}>
+			<div className='project-item-left'>
+				<p>{description1}</p>
+				<p>{description2}</p>
 
-                    {responsive ? <p>responsive</p> : <p>not responsive</p>}
-                </div>
-            </li>
-    )
+				<a href={githubLink} target='_blank'>
+					Github Link
+				</a>
+				<a href={siteLink} target='_blank'>
+					Site Link
+				</a>
+			</div>
+			<div className='project-item-right'>
+				<h2>{name}</h2>
+				<ul className='project-tech-tags'>{techItems}</ul>
+				<img src={image} alt='`screenshot of project ${props.name}`' />
+				{responsive ? <p>responsive</p> : <p>not responsive</p>}
+			</div>
+			{/* <div
+				className='project-img'
+				style={{ backgroundImage: `url("${image}")` }}></div> */}
+		</li>
+	)
 }
 
 export default Project
