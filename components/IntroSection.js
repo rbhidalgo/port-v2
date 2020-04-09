9e2894e53175e182146eb384d1fe07b2bb2e378e
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-08 22:03:00 -0700

    pass on scroll animation  props to introSection.js and ainmate svgs

--- components/IntroSection.js
+++ components/IntroSection.js
@@ -1,104 +1,184 @@
 import React, { Component } from 'react'
-import { motion } from "framer-motion"
+import { motion, useViewportScroll, useTransform } from 'framer-motion'
 
 // import Link from 'next/link'
 
 class IntroSection extends Component {
+	state = {
+		x: '',
+		y: '',
+		x2: '',
+		y2: '',
+		rotate: '',
+		rotate2: '',
+	}
 
-    state = {
-        x: '',
-        y: '',
-        x2: '',
-        y2: '',
-        rotate: '',
-        rotate2: ''
-    }
+	onMouseOver = () => {
+		const x = 25 * Math.floor(Math.random() * 5) - 50
+		const y = 25 * Math.floor(Math.random() * 5) - 50
+		const x2 = 25 * Math.floor(Math.random() * 5) - 50
+		const y2 = 25 * Math.floor(Math.random() * 5) - 50
+		this.setState({
+			x: `${x}px`,
+			y: `${y}px`,
+			x2: `${x2}px`,
+			y2: `${y2}px`,
+			rotate: '0deg',
+		})
+		console.log(`x=${this.state.x} y=${this.state.y}`)
+	}
 
-    onMouseOver = () => {
-        const x = 25 * (Math.floor(Math.random() * 5)) - 50
-        const y = 25 * (Math.floor(Math.random() * 5)) - 50
-        const x2 = 25 * (Math.floor(Math.random() * 5)) - 50
-        const y2 = 25 * (Math.floor(Math.random() * 5)) - 50
-        this.setState({
-            x: `${x}px`,
-            y: `${y}px`,
-            x2: `${x2}px`,
-            y2: `${y2}px`,
-            rotate: '0deg'
-        })
-        console.log(`x=${this.state.x} y=${this.state.y}`)
-    }
+	onMouseOut = () => {
+		this.setState({
+			x: `0px`,
+			y: `0px`,
+			x2: '0px',
+			y2: '0px',
+			rotate: '-10deg',
+			rotate2: '10deg',
+		})
+		console.log(this.state.x)
+	}
 
-    onMouseOut = () => {
-        this.setState({
-            x: `0px`,
-            y: `0px`,
-            x2: '0px',
-            y2: '0px',
-            rotate: '-10deg',
-            rotate2: '10deg'
-        })
-        console.log(this.state.x)
-    }
+	render() {
+		const { scale } = this.props
+		return (
+			<div className='wrapper'>
+				<section className='intro grid'>
+					<div className='intro-left-container'>
+						<motion.p
+							className='headline-body primary-light'
+							animate={{ opacity: 1 }}
+							transition={{ delay: 0.2 }}
+							initial={{ opacity: 0 }}>
+							Rogelio Hidalgo, Full Stack
+						</motion.p>
+						<h1>Web Developer</h1>
+						<p className='headline-body primary-dark'>
+							with a creative edge and a eye for UI/UX
+						</p>
+						<p className='max-width primary-light'>
+							focusing on front-end development with the ability to be flexible
+							and adapt to various projects. My knowledge in full-stack
+							development paired with my experience in graphic design allows me
+							to build applications that are aesthetically pleasing and
+							functional. I look forward to working with a team that values
+							continual growth and development.
+						</p>
 
-    
+						<motion.a
+							whileTap={{ scale: 0.8 }}
+							href='#'
+							className='primary-btn mt-48'>
+							get in touch
+						</motion.a>
+						<motion.a
+							whileTap={{ scale: 0.8 }}
+							href='#'
+							className='primary-btn mt-48'>
+							view my portfolio
+						</motion.a>
+						<span className='down-arrow'>
+							<img
+								src='/img/intro-section-downarrow.svg'
+								alt='down arrow'
+								height='20'
+								width='22'
+							/>
+						</span>
+					</div>
+					<motion.img
+						src='/img/rogelio-photo-bio.jpg'
+						className='top-img'
+						onMouseOver={this.onMouseOver}
+						onMouseOut={this.onMouseOut}
+						initial={{ x: 60, opacity: 0 }}
+						animate={{ x: 0, opacity: 1 }}
+						whileHover={{ opacity: 0, x: 85, zIndex: 1, delay: 0 }}
+						transition={{ delay: 0.2 }}
+					/>
+					<motion.img
+						src='/img/intro-section-rect1.svg'
+						className='rectangle'
+						style={{ scale }}
+						height='64'
+						width='64'
+					/>
 
-    render() {
-        return (
-            <div className="wrapper">
-            <section className="intro grid">
-                <div className="intro-left-container">
-                    <motion.p className="headline-body primary-light"  
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 0.2 }}
-                    initial={{ opacity: 0 }}
-                    >Rogelio Hidalgo, Full Stack
-                    </motion.p>
-                    <h1>Web Developer</h1>
-                    <p className="headline-body primary-dark">with a creative edge and a eye for UI/UX</p>
-                    <p className="max-width primary-light">focusing on front-end development with the ability to be flexible and adapt to various projects. My knowledge in full-stack development paired with my experience in graphic design allows me to build applications that are aesthetically pleasing and functional. I look forward to working with a team that values continual growth and development.</p>
-                    
-                    <motion.a
-                    whileTap={{ scale: 0.8 }}
-                    href="#" className="primary-btn mt-48">get in touch</motion.a>
-                    <a href="#" className="primary-btn mt-48">view my portfolio</a>
-                    <span className="down-arrow"><img src="/img/intro-section-downarrow.svg" alt="down arrow" height="20" width="22"/></span>
-                </div>
-                <motion.img src="/img/rogelio-photo-bio.jpg" className="top-img" onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut} 
-                initial={{ x: 60, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                whileHover={{ opacity: 0, x: 85, zIndex: 1, delay: 0 }}
-                transition={{ delay: 0.2 }}/
-                >
-                 <img src="/img/intro-section-rect1.svg" className="rectangle" height="64" width="64" />
+					<motion.img
+						src='/img/intro-section-poly02.svg'
+						className='intro-top-poly'
+						style={{ scale }}
+						height='62'
+						width='72'
+					/>
 
-                 <img src="/img/intro-section-poly02.svg" className="intro-top-poly" height="62" width="72" />
+					<motion.img
+						src='/img/intro-section-poly01.svg'
+						className='intro-btm-poly'
+						style={{ scale }}
+						height='62'
+						width='72'
+					/>
 
-                 <img src="/img/intro-section-poly01.svg" className="intro-btm-poly" height="62" width="72" />
+					<img
+						src='/img/cookie-doggie.jpg'
+						className='mid-img'
+						style={{
+							transform: `rotate(${this.state.rotate}) translate(${this.state.x}, ${this.state.y})`,
+						}}
+					/>
 
-                <img src="/img/cookie-doggie.jpg" className="mid-img"
+					<img
+						src='/img/odie-doggie.jpg'
+						className='btm-img'
+						style={{
+							transform: `rotate(${this.state.rotate2}) translate(${this.state.x2}, ${this.state.y2})`,
+						}}
+					/>
 
-                    style={{ transform: `rotate(${this.state.rotate}) translate(${this.state.x}, ${this.state.y})` }} />
+					<div className='intro-bkg-container'>
+						<motion.img
+							src='/img/intro-section-uparrow.svg'
+							className='intro-up-arrow'
+							style={{ scale }}
+							height='36'
+							width='48'
+						/>
+						<motion.img
+							src='/img/intro-section-uparrow.svg'
+							className='intro-up-arrow'
+							style={{ scale }}
+							height='36'
+							width='48'
+						/>
+						<motion.img
+							src='/img/intro-section-uparrow.svg'
+							className='intro-up-arrow'
+							style={{ scale }}
+							height='36'
+							width='48'
+						/>
+						<div className='intro-img-bkg'></div>
+						<img
+							src='/img/intro-section-ellipse01.svg'
+							className='circle spinning'
+							style={{ scale }}
+							height='81'
+							width='81'
+						/>
 
-                <img src="/img/odie-doggie.jpg" className="btm-img"
-
-                    style={{ transform: `rotate(${this.state.rotate2}) translate(${this.state.x2}, ${this.state.y2})` }} />
-
-
-                <div className="intro-bkg-container">
-                <img src="/img/intro-section-uparrow.svg" className="intro-up-arrow" height="36" width="48" />
-                <img src="/img/intro-section-uparrow.svg" className="intro-up-arrow" height="36" width="48" />
-                <img src="/img/intro-section-uparrow.svg" className="intro-up-arrow" height="36" width="48" />
-                    <div className="intro-img-bkg"></div>
-                    <img src="/img/intro-section-ellipse01.svg" className="circle spinning" height="81" width="81" />
-                   
-                    <img src="/img/intro-section-ellipse01.svg" className="big-circle spinning" height="111" width="111" />
-                </div>
-            </section>
-            </div>
-        )
-    }
+						<img
+							src='/img/intro-section-ellipse01.svg'
+							className='big-circle spinning'
+							height='111'
+							width='111'
+						/>
+					</div>
+				</section>
+			</div>
+		)
+	}
 }
-
 
 export default IntroSection
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { motion, useViewportScroll, useTransform } from "framer-motion"
+import { motion, useViewportScroll, useTransform } from 'framer-motion'
 
 import Layout from '../components/Layout'
 import Project from '../components/Project'
@@ -8,68 +8,70 @@ import projects from './projects.json'
 
 import Arrow from '../public/img/intro-section-downarrow.svg'
 
-
 const Index = () => {
+	const icon = {
+		hidden: {
+			pathLength: 0,
+			rotateZ: 0,
+			fill: 'rgba(255, 255, 255, 0)',
+		},
+		visible: {
+			pathLength: 1,
+			rotateZ: 360,
+			fill: 'rgba(255, 255, 255, 1)',
+		},
+	}
 
-    const icon = {
-        hidden: {
-          pathLength: 0,
-          rotateZ: 0,
-          fill: "rgba(255, 255, 255, 0)"
-        },
-        visible: {
-          pathLength: 1,
-          rotateZ: 360,
-          fill: "rgba(255, 255, 255, 1)"
-        }
-      }
-    
-      const { scrollYProgress } = useViewportScroll()
-      const scale = useTransform(
-          scrollYProgress, [0, 1], [0.2, 1.2]);
+	const { scrollYProgress } = useViewportScroll()
+	const scale = useTransform(scrollYProgress, [0, 1], [1.05, 0])
 
-    return(
-    <motion.div initial='initial' animate='animate' exit={{ opacity: 0 }}>
+	return (
+		<motion.div initial='initial' animate='animate' exit={{ opacity: 0 }}>
+			<Layout title='Full Stack Web Developer | Rogelio Hidalgo | Los Angeles, CA'>
+				<IntroSection scale={scale} />
+				<Skills />
+				<section className='projects-container wrapper'>
+					<div className='section-title grid'>
+						<h1 className='tt-uc'>Portfolio</h1>
+						<Arrow />
 
-    <Layout title="Full Stack Web Developer | Rogelio Hidalgo | Los Angeles, CA">
-   <IntroSection />
-   <Skills />
-    <section className="projects-container wrapper">
-    <div className="section-title grid">
-    <h1 className="tt-uc">Portfolio</h1>
-    <Arrow />
+						<motion.svg
+							style={{ scale }}
+							width={17.953}
+							height={24.562}
+							viewBox='0 0 17.953 24.562'>
+							<motion.path
+								d='M1 1h8.171v22.162L1 12.733h15.953L9.171 23.162'
+								stroke='#36eb80'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth={2}
+								variants={icon}
+								initial='hidden'
+								animate='visible'
+							/>
+						</motion.svg>
 
-    <motion.svg style={{ scale }} width={17.953} height={24.562} viewBox="0 0 17.953 24.562">
-      <motion.path
-        d="M1 1h8.171v22.162L1 12.733h15.953L9.171 23.162"
-        stroke="#36eb80"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        variants={icon}
-      initial="hidden"
-      animate="visible"
-      />
-    </motion.svg>
-
-    <svg width={81} height={81} viewBox="0 0 81 81">
-      <g fill="none" stroke="#a4ebc2" strokeWidth={2} strokeDasharray={25}>
-        <circle cx={40.5} cy={40.5} r={40.5} stroke="none" />
-        <circle cx={40.5} cy={40.5} r={39.5} />
-      </g>
-    </svg>
-
-    </div>
-        <ul className="grid">
-    {projects.map((project, index) => {
-    return <Project key={index} {...project} />
-        })}
-        </ul>
-    </section>
-    </Layout>
-    </motion.div>
-    
-    )}
-
+						<svg width={81} height={81} viewBox='0 0 81 81'>
+							<g
+								fill='none'
+								stroke='#a4ebc2'
+								strokeWidth={2}
+								strokeDasharray={25}>
+								<circle cx={40.5} cy={40.5} r={40.5} stroke='none' />
+								<circle cx={40.5} cy={40.5} r={39.5} />
+							</g>
+						</svg>
+					</div>
+					<ul className='grid'>
+						{projects.map((project, index) => {
+							return <Project key={index} {...project} />
+						})}
+					</ul>
+				</section>
+			</Layout>
+		</motion.div>
+	)
+}
 
 export default Index
