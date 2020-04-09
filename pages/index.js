cd26b09fa269b599e2fe535aafd036216f6e61c8
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-08 22:57:20 -0700

    update styling on arrow for Portfolio Heading

--- pages/index.js
+++ pages/index.js
@@ -33,35 +33,9 @@ const Index = () => {
 				<section className='projects-container wrapper'>
 					<div className='section-title grid'>
 						<h1 className='tt-uc'>Portfolio</h1>
-						<Arrow />
-
-						<motion.svg
-							style={{ scale }}
-							width={17.953}
-							height={24.562}
-							viewBox='0 0 17.953 24.562'>
-							<motion.path
-								d='M1 1h8.171v22.162L1 12.733h15.953L9.171 23.162'
-								stroke='#36eb80'
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth={2}
-								variants={icon}
-								initial='hidden'
-								animate='visible'
-							/>
-						</motion.svg>
-
-						<svg width={81} height={81} viewBox='0 0 81 81'>
-							<g
-								fill='none'
-								stroke='#a4ebc2'
-								strokeWidth={2}
-								strokeDasharray={25}>
-								<circle cx={40.5} cy={40.5} r={40.5} stroke='none' />
-								<circle cx={40.5} cy={40.5} r={39.5} />
-							</g>
-						</svg>
+						<span className='down-arrow-center'>
+							<Arrow />
+						</span>
 					</div>
 					<ul className='grid'>
 						{projects.map((project, index) => {
