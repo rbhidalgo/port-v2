5462c13a0396058d559302693c07cd024f2fab72
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-11 23:44:23 -0700

    updat link styling in footer component

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 const Footer = () => (
 	<footer>
 		<div className='wrapper grid'>
+			<hr className='mt-120' />
 			<a href='mailto:hello@erhidalgo.com' className='secondary-btn'>
 				email me
 			</a>
