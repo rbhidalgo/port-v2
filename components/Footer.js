b6e5a52676e5659551aa0d305959a30be03fe232
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-11 23:02:07 -0700

    update email in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,11 @@
 import Link from 'next/link'
 
-
 const Footer = () => (
-    <footer>
-        <a href="mailto:someone@example.com">
-  email me</a>
-        
-<style jsx>{`
-    
-`}</style>
-    </footer>
+	<footer>
+		<a href='mailto:hello@erhidalgo.com'>email me</a>
+
+		<style jsx>{``}</style>
+	</footer>
 )
 
-export default Footer;
+export default Footer
