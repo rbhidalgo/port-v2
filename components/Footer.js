cf2d25b85a10115fe67a29076812260877d5feb3
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-12 00:09:20 -0700

    update link styling in footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,28 @@ import Link from 'next/link'
 
 const Footer = () => (
 	<footer>
-		<div className='wrapper grid'>
-			<hr className='mt-120' />
+		<div className='wrapper grid mb-120'>
+			<div className='footer-arrows mt-64'>
+				<img
+					src='/img/intro-section-uparrow.svg'
+					className='footer-down-arrow'
+					height='36'
+					width='48'
+				/>
+				<img
+					src='/img/intro-section-uparrow.svg'
+					className='footer-down-arrow'
+					height='36'
+					width='48'
+				/>
+				<img
+					src='/img/intro-section-uparrow.svg'
+					className='footer-down-arrow'
+					height='36'
+					width='48'
+				/>
+			</div>
+			<hr />
 			<a href='mailto:hello@erhidalgo.com' className='secondary-btn'>
 				email me
 			</a>
