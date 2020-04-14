4b2311577e2f80d3b93cfaacaf4ddc8742b64603
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-13 22:34:45 -0700

    add styling for h4 and update styling for project tags

--- components/Project.js
+++ components/Project.js
@@ -26,21 +26,21 @@ const Project = ({
 
 	return (
 		<li className='project-item mb-64' key={index}>
-			<h5>{name}</h5>
+			<h4>{name}</h4>
 
 			<img src={image} alt='`screenshot of project ${props.name}`' />
 			<ul className='project-tech-tags'>{techItems}</ul>
 
 			<p>{description1}</p>
 			<p>{description2}</p>
-
+			{/* <h5>View</h5> */}
 			<a href={githubLink} className='primary-btn' target='_blank'>
 				View Code
 			</a>
 			<a href={siteLink} className='primary-btn' target='_blank'>
 				View Site
 			</a>
-			{responsive ? <p>responsive</p> : <p>not responsive</p>}
+			{/* {responsive ? <p>responsive</p> : <p>not responsive</p>} */}
 			{/* <div
 				className='project-img'
 				style={{ backgroundImage: `url("${image}")` }}></div> */}
