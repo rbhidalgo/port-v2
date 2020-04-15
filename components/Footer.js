c08d32d0ec08cd8fd1d5b7434460034f8760d35a
Rogelio Hidalgo <rhidalgo987@gmail.com>
2020-04-14 23:29:42 -0700

    add link to resume and update links in footer

--- components/Footer.js
+++ components/Footer.js
@@ -11,33 +11,35 @@ const Footer = () => {
 			<div className='wrapper grid mb-120'>
 				<div className='footer-arrows mt-64'>
 					<img
-						src='/img/intro-section-uparrow.svg'
+						src='/img/intro-section-up-arrow.svg'
 						className='footer-down-arrow'
 						height='36'
 						width='48'
 					/>
 					<img
-						src='/img/intro-section-uparrow.svg'
+						src='/img/intro-section-up-arrow.svg'
 						className='footer-down-arrow'
 						height='36'
 						width='48'
 					/>
 					<img
-						src='/img/intro-section-uparrow.svg'
+						src='/img/intro-section-up-arrow.svg'
 						className='footer-down-arrow'
 						height='36'
 						width='48'
 					/>
 				</div>
 				<hr />
-				<a href='mailto:hello@erhidalgo.com' className='secondary-btn'>
+				<a href='mailto:hello@rhidalgo.com' className='secondary-btn'>
 					email me
 				</a>
-				<a href='mailto:hello@erhidalgo.com' className='secondary-btn'>
+				<a
+					href='https://rhidalgo.com/assets/images/Rogelio_Hidalgo_Resume_fwd.pdf'
+					className='secondary-btn'>
 					View my Resume
 				</a>
 				<img
-					src='/img/intro-section-uparrow.svg'
+					src='/img/intro-section-up-arrow.svg'
 					className='to-top-btn'
 					onClick={scrollToTop}
 				/>
